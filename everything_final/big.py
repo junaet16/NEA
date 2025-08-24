c3092bffd77b48d9de3d90842df8d00482e289df
Junaet <junaetchowdhury@gmail.com>
2025-08-24 23:23:55 +0100

    Basic Dijkstra_With_Penalty created

--- everything_final/big.py
+++ everything_final/big.py
@@ -1,5 +1,8 @@
 import sqlite3
 import requests
+import math
+import collections
+import heapq
 
 API_KEY = "0ff5a2076cd640cb957e63d6947efc61"
 LINE_NAMES = [
@@ -23,7 +26,9 @@ LINE_NAMES = [
     "weaver",
     "suffragette"
     ]
-TRAVEL_TIME_DEFAULT = 3
+TRAVEL_TIME_DEFAULT = 3 #Will change this by adding coordinates of stations to the database and figuring out a more realistic time between stations
+TIME_PENALTY_DEFAULT = 5
+
 
 def create_tables():
     connection = sqlite3.connect("nea.db")
@@ -163,14 +168,65 @@ def Make_Graph():
     connection = sqlite3.connect("nea.db")
     cursor = connection.cursor()
     listOfNeighbours = {}
-    rows = cursor.execute("SELECT station_a, station_b, travel_time FROM connections").fetchall()
-    for station_a, station_b, travel_time in rows:
+    rows = cursor.execute("SELECT station_a, station_b, travel_time, line_id FROM connections").fetchall()
+    for station_a, station_b, travel_time, line_id in rows:
         if station_a not in listOfNeighbours:
             listOfNeighbours[station_a] = []
-        if [station_b, travel_time] not in listOfNeighbours[station_a]:
-            listOfNeighbours[station_a].append([station_b, travel_time])
+        if [station_b, travel_time, line_id] not in listOfNeighbours[station_a]:
+            listOfNeighbours[station_a].append([station_b, travel_time, line_id])
     connection.close()
     return listOfNeighbours
+
+
+def Dijkstra_With_Penalty(listOfNeighbours, start, goal):
+    distances = collections.defaultdict(lambda: math.inf)
+    previous = {}
+    visited = set()
+    startState = (start, None)
+    distances[startState] = 0
+    minHeap = [(0, startState)]
+    goalState = None
+
+    while minHeap:
+        currentCost, (currentStationID, currentLine) = heapq.heappop(minHeap)
+        if (currentStationID, currentLine) in visited:
+            continue
+        else:
+            visited.add((currentStationID, currentLine))
+
+        if currentStationID == goal:
+            goalState = (currentStationID, currentLine)
+            break
+
+        for nextStationID, nextTime, nextLine in listOfNeighbours[currentStationID]:
+            if currentLine is not None and currentLine != nextLine:
+                nextTime += TIME_PENALTY_DEFAULT
+
+            newCost = currentCost + nextTime
+            neighbourState = (nextStationID, nextLine)
+
+            if newCost < distances[neighbourState]:
+                distances[neighbourState] = newCost
+                previous[neighbourState] = (currentStationID, currentLine, nextLine)
+                heapq.heappush(minHeap, (newCost, neighbourState))
+
+    steps = []
+    stationPath = []
+
+    currentState = goalState
+    stationPath.append(currentState[0])
+
+    while currentState != startState:
+        previousStationID, previousLineID, lineUsed = previous[currentState]
+        currentStationID, currentLineID = currentState
+        steps.append((previousStationID, currentStationID, lineUsed, distances[(currentStationID, currentLineID)]))
+        stationPath.append(previousStationID)
+        currentState = (previousStationID, previousLineID)
+
+    steps.reverse()
+    stationPath.reverse()
+
+    return stationPath, steps
 
 
 if __name__ == "__main__":
@@ -178,6 +234,5 @@ if __name__ == "__main__":
     #stationDictionary = fetch_stations()
     #linesDictionary = fetch_lines(stationDictionary)
     #Save_TfL_Data(stationDictionary, linesDictionary)
-    listOfNeighbours = Make_Graph()
-    print(listOfNeighbours)
+    #listOfNeighbours = Make_Graph()
     print("done")
--- test_concepts/path.py
+++ test_concepts/path.py
@@ -1,5 +1,8 @@
+import heapq
 import sqlite3
 import requests
+import math
+import collections
 
 API_KEY = "0ff5a2076cd640cb957e63d6947efc61"
 LINE_NAMES = [
@@ -24,6 +27,8 @@ LINE_NAMES = [
     "suffragette"
     ]
 TRAVEL_TIME_DEFAULT = 3
+TIME_PENALTY_DEFAULT = 5
+
 
 
 def Make_Graph():
@@ -39,7 +44,63 @@ def Make_Graph():
     connection.close()
     return listOfNeighbours
 
+
+def Dijkstra_With_Penalty(listOfNeighbours, start, goal):
+    distances = collections.defaultdict(lambda: math.inf)
+    previous = {}
+    visited = set()
+    startState = (start, None)
+    distances[startState] = 0
+    minHeap = [(0, startState)]
+    goalState = None
+
+    while minHeap:
+        currentCost, (currentStationID, currentLine) = heapq.heappop(minHeap)
+        if (currentStationID, currentLine) in visited:
+            continue
+        else:
+            visited.add((currentStationID, currentLine))
+
+        if currentStationID == goal:
+            goalState = (currentStationID, currentLine)
+            break
+
+        for nextStationID, nextTime, nextLine in listOfNeighbours[currentStationID]:
+            if currentLine is not None and currentLine != nextLine:
+                nextTime += TIME_PENALTY_DEFAULT
+
+            newCost = currentCost + nextTime
+            neighbourState = (nextStationID, nextLine)
+
+            if newCost < distances[neighbourState]:
+                distances[neighbourState] = newCost
+                previous[neighbourState] = (currentStationID, currentLine, nextLine)
+                heapq.heappush(minHeap, (newCost, neighbourState))
+
+    steps = []
+    stationPath = []
+
+    currentState = goalState
+    stationPath.append(currentState[0])
+
+    while currentState != startState:
+        previousStationID, previousLineID, lineUsed = previous[currentState]
+        currentStationID, currentLineID = currentState
+        steps.append((previousStationID, currentStationID, lineUsed, distances[(currentStationID, currentLineID)]))
+        stationPath.append(previousStationID)
+        currentState = (previousStationID, previousLineID)
+
+    steps.reverse()
+    stationPath.reverse()
+
+    print(steps)
+    print(stationPath)
+
+    return stationPath, steps
+
+
+
 if __name__ == "__main__":
     listOfNeighbours = Make_Graph()
-    print(listOfNeighbours)
+    stationPath, steps = Dijkstra_With_Penalty(listOfNeighbours, "940GZZLULYS", "HUBEAL")
     print("done")
