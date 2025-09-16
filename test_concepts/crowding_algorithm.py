bf01ff9536345623aaee5eb605cf5f995bbc0153
junaet16 <junaetchowdhury@gmail.com>
2025-09-16 13:06:30 +0100

    dijkstras and graph function needs to be changed

--- test_concepts/crowding_algorithm.py
+++ test_concepts/crowding_algorithm.py
@@ -1,13 +1,26 @@
-#Creating temporary fake data for testing purposes
 import sqlite3
+
+
+
+
+#I realised I need to fundamentally change the Dijkstra's and making graph functions soooooooooooooooooo thats the task for at home :sob
+
+
 
 def fake_events_and_stadium():
     connection = sqlite3.connect('nea.db')
     cursor = connection.cursor()
     #Use JSON instead of a list of stuff?
     #Create tables
+
+    kill = ["DROP TABLE IF EXISTS venues", "DROP TABLE IF EXISTS events"]
+
+    for thingToKill in kill:
+        cursor.execute(thingToKill)
+
+
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS venue (
+    CREATE TABLE IF NOT EXISTS venues (
         nameID TEXT PRIMARY KEY,
         venueName TEXT,
         teamID TEXT,
@@ -25,15 +38,16 @@ def fake_events_and_stadium():
     homeTeamID TEXT,
     awayTeamID TEXT,
     PRIMARY KEY(venueID, dateOfEvent)
-    FOREIGN KEY(venueID) REFERENCES venue(nameID)
+    FOREIGN KEY(venueID) REFERENCES venues(nameID)
     )""")
+#Test
 
     venuesList = [
         ("Emirates", "Emirates Stadium", "ARS", 60704, "940GZZLUASL", "N/A")
     ]
 
     cursor.executemany("""
-    INSERT INTO venue (nameID, venueName, teamID, capacity, stationsNearby, coordinates)
+    INSERT INTO venues (nameID, venueName, teamID, capacity, stationsNearby, coordinates)
     VALUES (?, ?, ?, ?, ?, ?)""", venuesList)
 
     eventsList = [
@@ -55,7 +69,14 @@ def takeInput():
     if input("Use custom? y/n: ") == "y":
         pass #late
     else:
-        pass #Will work on it later
+        start = "940GZZLULYS"
+        end = "940GZZLUBOS"
+        date = "23/12/2025"
+        starTime = "14:00"
+
+
+def calculateMidJourney(unprocessedInput):
+    pass
 
 
 
@@ -67,6 +88,8 @@ def takeInput():
 
 def main():
     fake_events_and_stadium()
+    unprocessedInput = takeInput()
+    finalInput = calculateMidJourney(unprocessedInput)
 
 if __name__ == '__main__':
     main()
