8207b34d1b8d08223cbcc6351994d1f92957fdc3
junaet16 <junaet16@users.noreply.github.com>
2025-12-04 17:13:07 +0000

    FINALLY fixed the affectedOldPath stuff (basically I was accidentally multiplying the change time with the delay factor - whoopsie lmao) - current solution is to strip down the graph to only the connections in the original path and then just do dikstra on that - seems to be working for now :)

--- finalFiles/getResults.py
+++ finalFiles/getResults.py
@@ -3,6 +3,19 @@ import copy
 import pathFinding
 import customFunctions
 import classes
+import sqlite3
+
+
+def getNaPTAN(name, database):
+    connection = sqlite3.connect(database)
+    cursor = connection.cursor()
+
+    cursor.execute(f"""
+        SELECT NaPTAN
+        FROM Stations
+        WHERE StationName = ?""", (name,))
+    result = cursor.fetchall()[0][0]
+    return result
 
 
 def GetaffectedGraph(baseGraph, affectedNetwork):
@@ -22,29 +35,43 @@ def GetaffectedGraph(baseGraph, affectedNetwork):
     return affectedGraph
 
 
-def getAffectedOldPath(oldPath, affectedNetwork):
-    affectedOldPath = []
-    for station in oldPath:
-        key = (station[0], station[1], station[2])
-        connection = affectedNetwork.edges[key]
-        newTime = station[3] * connection.LineDelay
-        newData = (station[0], station[1], station[2], newTime)
-        affectedOldPath.append(newData)
+def getStrippedGraph(oldPath, affectedGraph):
+    strippedGraph = {}
 
-    return affectedOldPath
+    for connection in oldPath:
+        StationA = connection[0]
+        StationB = connection[1]
+        LineID = connection[2]
+
+        connectedStations = affectedGraph[StationA]
+        for connectedStation in connectedStations:
+            connectedStationB = connectedStation[0]
+            connectedLineID = connectedStation[2]
+            if connectedStationB == StationB and connectedLineID == LineID:
+                strippedGraph[StationA] = [connectedStation]
+
+    return strippedGraph
 
 
-def main(date, journeyStart, startStation, endStation, databaseFile, username):
+def getAffectedOldPath(oldPath, affectedGraph, startStation, endStation, CHANGING_TIME):
+    strippedGraph = getStrippedGraph(oldPath, affectedGraph)
+    returnPath = pathFinding.Dijkstra(strippedGraph, startStation, endStation, CHANGING_TIME)
+
+    return returnPath
+
+
+def main(date, journeyStart, startStationName, endStationName, databaseFile, username):
+    startStation = getNaPTAN(startStationName, databaseFile)
+    endStation = getNaPTAN(endStationName, databaseFile)
+
     affectedNetwork = crowdingAlgorithm.main(date, journeyStart, startStation, endStation, databaseFile, username)
     parameters = customFunctions.getParameters(username, databaseFile)
 
     baseGraph = pathFinding.MakeGraph(databaseFile)
     affectedGraph = GetaffectedGraph(baseGraph, affectedNetwork)
-    print(baseGraph)
-    print(affectedGraph)
 
     oldPath = pathFinding.Dijkstra(baseGraph, startStation, endStation, parameters.CHANGING_TIME)
-    affectedOldPath = getAffectedOldPath(oldPath, affectedNetwork)
+    affectedOldPath = getAffectedOldPath(oldPath, affectedGraph, startStation, endStation, parameters.CHANGING_TIME)
     newPath = pathFinding.Dijkstra(affectedGraph, startStation, endStation, parameters.CHANGING_TIME)
 
     affectedOldPathDesriptionObject = classes.Severity(affectedOldPath, username, databaseFile, startStation, affectedNetwork)
@@ -56,20 +83,21 @@ def main(date, journeyStart, startStation, endStation, databaseFile, username):
     newPathDescriptions = newPathDesptionObject.descriptions
 
     resultsObject = classes.Results(oldPath, newPath, affectedOldPath, newPathDescriptions, affectedOldPathDesriptions)
+    return resultsObject
+
+
+if __name__ == '__main__':
+    date = "03/12/2025"
+    journeyStart = "18:30"
+    startStation = "Leytonstone Underground Station"
+    endStation = "Stonebridge Park"
+    databaseFile = "final.db"
+    username = "Junaet"
+
+    resultsObject = main(date, journeyStart, startStation, endStation, databaseFile, username)
 
     print(resultsObject.oldPath)
     print(resultsObject.newPath)
     print(resultsObject.affectedOldPath)
     print(resultsObject.newPathDescriptions)
     print(resultsObject.affectedOldPathDesriptions)
-
-
-
-if __name__ == '__main__':
-    date = "03/12/2025"
-    journeyStart = "18:30"
-    startStation = "940GZZLULYS"
-    endStation = "HUBSRA"
-    databaseFile = "final.db"
-    username = "Junaet"
-    main(date, journeyStart, startStation, endStation, databaseFile, username)
